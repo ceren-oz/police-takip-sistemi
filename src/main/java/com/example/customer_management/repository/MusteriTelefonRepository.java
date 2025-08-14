aa30f4025a99305d57feadd9febba0f8e96823e8
betulbilhan <betulbilhan@gmail.com>
2025-08-14 17:35:04 +0300

    commit

--- src/main/java/com/example/customer_management/repository/MusteriTelefonRepository.java
+++ src/main/java/com/example/customer_management/repository/MusteriTelefonRepository.java
@@ -12,3 +12,17 @@ public interface MusteriTelefonRepository extends JpaRepository<MusteriTelefon,L
     //findByMusteriId ile bir müşteriye ait tüm telefonları listeleyebilirsin.
 //Spring Data JPA’nın metod adı ile sorgu oluşturma özelliği.
 }
+
+
+//NOT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+//JpaRepository zaten hazır CRUD metodları sunar:
+//
+//findById(id) → tek bir kaydı ID ile getirir.
+//
+//save(entity) → kaydı ekler veya günceller.
+//
+//deleteById(id) → ID’ye göre siler.
+//
+//Yani sen findById yazmasan bile JPA zaten sağlar.
+//
+//Bu metodları “kim yazdı” sorusu: Spring Data JPA ekibi yazdı, biz sadece interfaceimizi extends JpaRepository ile bağladık.
