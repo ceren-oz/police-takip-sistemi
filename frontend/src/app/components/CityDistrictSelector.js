35f053f1b7278a65ddaf7323f4f836f1d223802a
ceren-oz <ceren.oz@ozu.edu.tr>
2025-08-21 17:38:00 +0300

    small fix: CityDistrictSelector BACKEND_BASE fixed

--- frontend/src/app/components/CityDistrictSelector.js
+++ frontend/src/app/components/CityDistrictSelector.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
-//const BACKEND_BASE = "http://localhost:8080";
+const BACKEND_BASE = "http://localhost:8080";
 
 export default function CityDistrictSelector({
   city,
